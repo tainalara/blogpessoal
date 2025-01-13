f4ab6a6f2cf3193c89856fb5babe7ff90e563f84
Tainá Lara <tainalara.dev@gmail.com>
2025-01-13 12:53:51 -0300

    Implementação dos métodos de Postagem, Atualização e Delete. Além das buscas por ID e titulo

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,29 +1,77 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
 
-@RestController
-@RequestMapping("/postagens")
-@CrossOrigin (origins = "*", allowedHeaders = "*")
+import jakarta.validation.Valid;
+
+@RestController 									//Define ao Spring que essa Classe é uma Controller
+@RequestMapping("/postagens")						//Define qual endpoint vai ser tratado por essa Classe
+@CrossOrigin (origins = "*", allowedHeaders = "*")  //Libera o acesso através de determinado endereço(um fator de segurança) O Uso do * é um jeito de deixar em branco quando o endereço não é determinado
 public class PostagemController {
 
-	@Autowired
-	private PostagemRepository postagemRepository;
+	@Autowired // O Spring dá autonomia para a Interface poder invocar os métodos // Da acesso para variável 
+	private PostagemRepository postagemRepository; //É uma variável que representa a PostagemRepository Injeção de 
 	
-	@GetMapping
+	@GetMapping // Indica que esse método é chamado em Verbos/Métodos HTTP do tipo Get
 	public ResponseEntity<List<Postagem>> getAll(){
-		return ResponseEntity.ok(postagemRepository.findAll());
+		return ResponseEntity.ok(postagemRepository.findAll()); // È igual ao SELEC * FROM tb_postagens (mostrar tudo que esta salvo nesta tabela)
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id) {
+		return postagemRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(postagemRepository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
+		return postagemRepository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+						.body(postagemRepository.save(postagem)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus (HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Postagem> postagem = postagemRepository.findById(id);
+		
+		if(postagem.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		postagemRepository.deleteById(id);
+	}
 }
+
 
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,9 +1,23 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Postagem;
 
-public interface PostagemRepository  extends JpaRepository<Postagem, Long> {
+public interface PostagemRepository  extends JpaRepository<Postagem, Long> { 
+	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 
 }
+
+// Métodos abstratos que aparecem dentro da Interface
+
+// save (Objeto objeto)
+// findById (Long Id)
+// existById (Long Id)
+// findAll ()
+// deleteById(Long id)
+// deleteAll()
